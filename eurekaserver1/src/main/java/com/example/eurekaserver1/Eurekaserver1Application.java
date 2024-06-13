48b4f5bd94db97f85844e873b499500b34860a46
Octavio Sánchez Huerta <osanchezhuerta@gmail.com>
2024-06-13 15:36:51 -0600

    3

---
+++ eurekaserver1/src/main/java/com/example/eurekaserver1/Eurekaserver1Application.java
@@ -0,0 +1,15 @@
+package com.example.eurekaserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Eurekaserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eurekaserver1Application.class, args);
+	}
+
+}
